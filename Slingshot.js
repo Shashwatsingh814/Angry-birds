44a9cfd4ccb49a3c2d7aa89942db605f0ae172bc
Shashwatsingh814 <83904742+Shashwatsingh814@users.noreply.github.com>
2021-07-03 17:27:16 +0700

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,13 @@
+Class SlingShot{
+
+    constructor(bodyA,pointb)
+}
+ this.pointb;
+ this.sling=Coistraint.create(ab);
+ World.add(myworld,this.sling);
+ pointA= bodyA.position;
+ pointb=this.sling.pointb;
+
+ push();
+ strokeWeight(5);
+ line(pointa.s,pointb.x,pointb.y)
